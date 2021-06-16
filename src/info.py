92bfa17c403d0a9d760df2f8e1eb22db12d90711
Rafael <85809040+rafaeluy@users.noreply.github.com>
2021-06-15 22:10:59 -0300

    Update info.py

--- src/info.py
+++ src/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "RebornOS Installer Gnome based 2021.06.15"
+CNCHI_VERSION = "RebornOS Installer Gnome based 2021.06.16"
 CNCHI_WEBSITE = "https://rebornos.org"
 CNCHI_RELEASE_STAGE = "production"
 
