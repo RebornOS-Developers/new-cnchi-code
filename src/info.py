7ccc0bcb4eb597a2e368a8bf760e77ce58ce1dcb
Rafael <85809040+rafaeluy@users.noreply.github.com>
2021-09-08 11:06:47 -0300

    Change version number

--- src/info.py
+++ src/info.py
@@ -29,7 +29,7 @@
 
 """ Set some Cnchi global constants """
 
-CNCHI_VERSION = "RebornOS Installer Gnome based 2021.06.30"
+CNCHI_VERSION = "RebornOS Installer Gnome based 2021.09.08"
 CNCHI_WEBSITE = "https://rebornos.org"
 CNCHI_RELEASE_STAGE = "production"
 
